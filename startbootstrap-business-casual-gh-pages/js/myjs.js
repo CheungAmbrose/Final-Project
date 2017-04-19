5a9a1097178128eccfca8650a5f76e8a8c6893dc
Ben LaForge <benlaforge@gmail.com>
2017-04-18 20:14:05 -0400

    Big change: no more nav bar, user selects which sections to take

--- startbootstrap-business-casual-gh-pages/js/myjs.js
+++ startbootstrap-business-casual-gh-pages/js/myjs.js
@@ -6,3 +6,59 @@ function printFunc()
 	doc.text("Primary Housing Type: " + input, 10, 20)
 	doc.save('test.pdf')
 }
+
+window.onload = function() {
+    document.getElementById('issuesentence').style.display = 'none';
+    document.getElementById('issueentry').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('mydemographics').style.display = 'none';
+    document.getElementById('myorganization').style.display = 'none';
+    document.getElementById('myissue').style.display = 'none';
+    document.getElementById('myengagement').style.display = 'none';
+}
+
+function yesnoCheckissue() {
+    if (document.getElementById('issq1y').checked) {
+        document.getElementById('issuesentence').style.display = 'block';
+        document.getElementById('issueentry').style.display = 'block';
+    } 
+    else if(document.getElementById('issq1n').checked) {
+        document.getElementById('issuesentence').style.display = 'none';
+        document.getElementById('issueentry').style.display = 'none';
+   }
+}
+
+function yesnoCheckorg() {
+    if (document.getElementById('orgq3y').checked) {
+        document.getElementById('contact').style.display = 'block';
+    } 
+    else if(document.getElementById('orgq3n').checked) {
+        document.getElementById('contact').style.display = 'none';
+   }
+}
+
+function sectionCheck() {
+	if (document.getElementById('demographicscheck').checked) {
+        document.getElementById('mydemographics').style.display = 'block';
+    } else if(!document.getElementById('demographicscheck').checked) {
+        document.getElementById('mydemographics').style.display = 'none';
+    }
+
+    if(document.getElementById('organizationcheck').checked) {
+        document.getElementById('myorganization').style.display = 'block';
+    } else if(!document.getElementById('organizationcheck').checked) {
+        document.getElementById('myorganization').style.display = 'none';
+    }
+
+    if(document.getElementById('issuecheck').checked) {
+        document.getElementById('myissue').style.display = 'block';
+    } else if(!document.getElementById('issuecheck').checked) {
+        document.getElementById('myissue').style.display = 'none';
+    }
+
+    if(document.getElementById('engagementcheck').checked) {
+        document.getElementById('myengagement').style.display = 'block';
+    } else if(!document.getElementById('engagementcheck').checked) {
+        document.getElementById('myengagement').style.display = 'none';
+    }
+}
