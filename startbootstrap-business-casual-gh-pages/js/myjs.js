22bc1ca5dac9752624c438f3041da6453cc7efb1
Cheung, Ambrose Hin Wing <cheungambrose30@gmail.com>
2017-04-21 06:17:12 -0400

    Partial Progress Work
    Set up displaying partial progress

--- startbootstrap-business-casual-gh-pages/js/myjs.js
+++ startbootstrap-business-casual-gh-pages/js/myjs.js
@@ -10,11 +10,14 @@ function printFunc()
 window.onload = function() {
     document.getElementById('issuesentence').style.display = 'none';
     document.getElementById('issueentry').style.display = 'none';
+    document.getElementById('issq6optional').style.display = 'none';
+    document.getElementById('engq1').style.display = 'none';
     document.getElementById('contact').style.display = 'none';
     document.getElementById('mydemographics').style.display = 'none';
     document.getElementById('myorganization').style.display = 'none';
     document.getElementById('myissue').style.display = 'none';
     document.getElementById('myengagement').style.display = 'none';
+    sectionCheck();
 }
 
 function yesnoCheckissue() {
@@ -25,7 +28,14 @@ function yesnoCheckissue() {
     else if(document.getElementById('issq1n').checked) {
         document.getElementById('issuesentence').style.display = 'none';
         document.getElementById('issueentry').style.display = 'none';
-   }
+    }
+
+    if (document.getElementById('issq6y').checked) {
+        document.getElementById('issq6optional').style.display = 'block';
+    } 
+    else if(document.getElementById('issq6n').checked) {
+        document.getElementById('issq6optional').style.display = 'none';
+    }
 }
 
 function yesnoCheckorg() {
@@ -34,7 +44,22 @@ function yesnoCheckorg() {
     } 
     else if(document.getElementById('orgq3n').checked) {
         document.getElementById('contact').style.display = 'none';
-   }
+    }
+}
+
+function yesnoCheckeng() {
+    if (document.getElementById('engq1y').checked) {
+        document.getElementById('engq1').style.display = 'block';
+    } 
+    else if(document.getElementById('engq1n').checked) {
+        document.getElementById('engq1').style.display = 'none';
+    }
+}
+
+function checkAllInput() {
+    document.getElementById('orgq3fbtext').innerHTML = document.getElementById('contactinput').value;
+    document.getElementById('issq1fbtext').innerHTML = document.getElementById('issueentry').value;
+    document.getElementById('engq1fbtext').innerHTML = document.getElementById('engq1input').value;
 }
 
 function sectionCheck() {
